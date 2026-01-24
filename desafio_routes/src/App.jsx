d100ad156d4b591b5b3afe8a4f6635df7f8a6332
lilicesm <alicesm3012@gmail.com>
2026-01-24 13:30:25 -0300

    Update App.jsx

--- desafio_routes/src/App.jsx
+++ desafio_routes/src/App.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
 import Produtos from './components/Produtos';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
-  return <div>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Produtos />}></Route>
-      </Routes>
-    </BrowserRouter>
-  </div>
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Header />
+        <div className='content'></div>
+        <Routes>
+          <Route path="/" element={<Produtos />}></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>);
 };
 
 export default App;

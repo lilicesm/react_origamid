4d511b9d14a86cb9b26d38e825eba06ed7eb08e9
lilicesm <alicesm3012@gmail.com>
2026-01-24 13:17:46 -0300

    Header, Footer, Produtos

--- desafio_routes/src/App.jsx
+++ desafio_routes/src/App.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Produtos from './components/Produtos';
+
 const App = () => {
-  return <div></div>
+  return <div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Produtos />}></Route>
+      </Routes>
+    </BrowserRouter>
+  </div>
 };
 
 export default App;

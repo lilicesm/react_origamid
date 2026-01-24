2bb2caa019248cf33b37d93cc1db8d8e679c3658
lilicesm <alicesm3012@gmail.com>
2026-01-24 17:06:57 -0300

    contato

--- desafio_routes/src/components/Contato.jsx
+++ desafio_routes/src/components/Contato.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import styles from './Contato.module.css';
+import foto from '../img/contato.jpg';
 
 const Contato = () => {
   return (
-    <div className={styles.contato}>
-      <h1>Contato</h1>
-    </div>
+    <section className={styles.contato}>
+      <img src={foto} alt="Máquina de escrever" />
+      <div>
+        <h1>Entre em contato.</h1>
+        <ul className={styles.dados}>
+          <li>alice@alice.com</li>
+          <li>99999-9999</li>
+          <li>Aquela Rua, 111</li>
+        </ul>
+      </div>
+    </section>
   );
 }
 

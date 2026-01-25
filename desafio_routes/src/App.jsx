836925ad0216f760c6f0491d539716d107edf3ac
lilicesm <alicesm3012@gmail.com>
2026-01-25 11:47:24 -0300

    Redireciona p cada pagina de produto

--- desafio_routes/src/App.jsx
+++ desafio_routes/src/App.jsx
@@ -5,6 +5,7 @@ import Produtos from './components/Produtos';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Contato from './components/Contato';
+import Produto from './components/Produto';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <div className='content'></div>
         <Routes>
           <Route path="/" element={<Produtos />}></Route>
+          <Route path="/produto/:id" element={<Produto />}></Route>
           <Route path="contato" element={<Contato />}></Route>
         </Routes>
         <Footer />

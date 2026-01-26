d5ccd22719eb9f1ebfc399d91fe649853ac78a1b
lilicesm <alicesm3012@gmail.com>
2026-01-26 17:39:19 -0300

    testando rest api

--- projeto_final/src/App.jsx
+++ projeto_final/src/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import './App.css';
+import Api from './api/Api.jsx';
 
 const App = () => {
-  return <div>App React</div>;
+  return <div>
+    <Api />
+  </div>;
 };
 
 export default App;

117c9c74f49d138554db0bc447748287da565fde
lilicesm <alicesm3012@gmail.com>
2026-01-27 22:16:01 -0300

    header

--- projeto_final/src/Components/Header.jsx
+++ projeto_final/src/Components/Header.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import style from './Header.module.css';
 import { Link } from 'react-router-dom';
+import Dog from '../Assets/dog.svg?react';
 
 const Header = () => {
-  return <div className={style.header}>
-    <nav className='container'>
-      <Link to="/">Home</Link>
-      <span> </span>
-      <Link to="/login">Login / Cadastro</Link>
+  return <header className={style.header}>
+    <nav className={`${style.nav} container`}>
+      <Link className={style.logo} to="/" aria-label='Dogs - Home'>
+        <Dog />
+      </Link>
+      <Link className={style.login} to="/login">Login / Cadastro</Link>
     </nav>
-  </div>;
+  </header>;
 };
 
 export default Header;

3e84cd8eb2544a4fd8c74f7a4fdb52193b11b636
lilicesm <alicesm3012@gmail.com>
2026-02-03 09:43:00 -0300

    cadastro

--- projeto_final/src/Hooks/useForm.jsx
+++ projeto_final/src/Hooks/useForm.jsx
@@ -3,7 +3,11 @@ import React from "react";
 const types = {
   email: {
     regex: /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/ ,
-    message: 'Preencha um email válido'
+    message: 'Preencha um email válido.'
+  },
+  password : {
+    regex: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+    message: 'A senha deve ter no mínimo 8 caracteres, pelo menos uma letra e um número.'
   }
 }
 
@@ -14,7 +18,7 @@ const useForm = (type) => {
   function valida(value){
     if(type === false) return true;
     if(value.length === 0) {
-      setError('Preencha um valor');
+      setError('Preencha um valor.');
       return false;
     } else if(types[type] && !types[type].regex.test(value)){
       setError(types[type].message);
